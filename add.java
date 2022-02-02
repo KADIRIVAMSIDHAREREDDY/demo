5230e7d49df58039dc7ff02108e04dc07dcb1663
KADIRIVAMSIDHARREDDY <kadirivamsi1997@gmail.com>
2022-02-02 11:56:31 +0530

    Add my first file to repo

---
+++ add.java
@@ -0,0 +1,14 @@
+class add
+}
+public static void main(string args[])
+{
+int num1,num2,res;
+system.out.println("enter first number value");
+scanner s= new scanner(system.in);
+num1=s.nextInt();
+system.out.println("enter second number value");
+num2=s.nextInt();
+res=num1+num2;
+System.out.println("Result is "+res);
+}
+}
